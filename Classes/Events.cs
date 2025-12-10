51bd3c5fed4d518424d38d99a1e8745ef271de33
Ilia12 <q0645010@gmail.com>
2025-12-10 14:19:57 +0500

    Созданный классы для приложения. Кантуганов

---
+++ Classes/Events.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerTelegramBot_Кантуганов.Classes
+{
+    public class Events
+    {
+        public DateTime Time { get; set; }
+        public string Message { get; set; }
+        public Events(DateTime Time, string Message) {
+            this.Time = Time;
+            this.Message = Message;
+        }
+    }
+}
---
+++ Classes/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerTelegramBot_Кантуганов.Classes
+{
+    public class Users
+    {
+        public long IdUser { get; set; }
+        public List<Events> Events { get; set; }
+        public Users(long IdUser) {
+            this.IdUser = IdUser;
+            this.Events = new List<Events>();
+        }
+    }
+}
